8fc9c402c86667e3c258ab22c4476cf6c5cfc0cd
IsacZetterström <isac.zetterstrom@hotmail.se>
2023-10-10 13:17:54 +0200

    Added a function (edituser) that updates users details in DB.

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -1,15 +1,29 @@
 import connection from "../config/database.js";
 
-const User = {};
+// const User = {};
 
-User.GetAll = async () => {
-  try {
-    const result = await connection.promise().query("Select * from User");
-    return result;
-  } catch (error) {
-    // console.error('Error', error);
-    throw error;
-  }
-};
+// User.GetAll = async () => {
+//   try {
+//     const result = await connection.promise().query("Select * from User");
+//     return result;
+//   } catch (error) {
+//     // console.error('Error', error);
+//     throw error;
+//   }
+// };
 
-export default User;
+async function editUser(userid, email, firstname, lastname, phone) {
+  const sql = `UPDATE User SET
+    Email = IFNULL(?, email),
+    Firstname = IFNULL(?, firstName),
+    Lastname = IFNULL(?, lastname),
+    Phone = IFNULL(?, phone)
+    WHERE User_id = ?`;
+
+  const result = await connection
+    .promise()
+    .query(sql, [email, firstname, lastname, phone, userid]);
+  return result;
+}
+
+export default { editUser };
