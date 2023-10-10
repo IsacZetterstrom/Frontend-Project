b38fe0f3bb951a592ade3bf0c20bcf6f43b529f1
ledei <holybuzz@live.se>
2023-10-10 12:58:31 +0200

    added a controller for the /profile/user/bookings/:id route

--- Backend/src/router/privateRoutes.js
+++ Backend/src/router/privateRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
+import bookingController from "../controllers/bookingController.js";
 const privateRoutes = Router();
 
 privateRoutes.get("/user", userController.GetAll);
 privateRoutes.patch("/user");
 privateRoutes.get("/user/bookings");
-privateRoutes.delete("/user/bookings/:booking_id");
+privateRoutes.delete("/user/bookings/:booking_id",bookingController.delBooking);
 
 export default privateRoutes;
