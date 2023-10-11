cd00ddb274d8879613ce269003b87646866f32e1
KernelTestPilot <47748828+KernelTestPilot@users.noreply.github.com>
2023-10-11 13:34:10 +0200

    testsets

--- Backend/src/services/jwtService.js
+++ Backend/src/services/jwtService.js
@@ -8,14 +8,11 @@ async function generateToken(email){
     }
     let token = jwt.sign(payload, process.env.SUPER_SECRET,{
       expiresIn: "15m",});
-      console.log(token)
       return token;
   }
 
   async function verifyToken(req, res, next) {
-    const token =
-      req.body.token || req.query.token || req.headers["x-access-token"];
-  
+    const token = req.headers["x-access-token"];
     if (!token) {
       return res.status(403).send("A token is required for authentication");
     }
