d38e48cf21de2b5b3b4afcc5d59be92f5d9f25da
lollocodes <louise_johansson.94@hotmail.com>
2023-10-11 15:23:00 +0200

    added controller get a specific movie

---
+++ Backend/src/controllers/movieController.js
@@ -0,0 +1,19 @@
+import movieModel from "../models/movieModel.js";
+
+async function getOneMovie(req, res) {
+  try {
+      const { movie_id } = req.params;
+      const movieData = await movieModel.getMovieInformation(movie_id);
+      const movie = movieData[0];
+
+      if (movie.length > 0) {
+        res.json({ status: 200, message: "Movie found", movie });
+      } else {
+        res.status(404).json({ error: "Movie not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+export default { getOneMovie };
