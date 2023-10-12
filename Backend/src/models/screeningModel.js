dcaf444de0c1511ec1878d39ab13f41210786c9b
Oliver Andersson <oliver.andersson101@gmail.com>
2023-10-12 14:41:11 +0200

    Added comments

--- Backend/src/models/screeningModel.js
+++ Backend/src/models/screeningModel.js
@@ -1,17 +1,23 @@
 import connection from "../config/database.js";
 
-// Fetch all screenings for a specific movie
+/*
+Author: Oliver Andersson
+Description:
+Models for fetching screening data from database
+*/
+
+// Fetch all screenings for a specific movie using movieID
 async function getScreenings(movieID) {
   try {
     
     const result = await connection.promise().query(`
-    SELECT Screening.Screening_id, Movie.Title, Theater.Theater_name,
-    Screening.Screening_startime, Screening.Screening_date
-    
-    FROM Screening
-    INNER JOIN Theater ON Theater.Theater_id = Screening.Theater_id
-    INNER JOIN Movie ON Movie.Movie_id = Screening.Movie_id
-    WHERE Screening.movie_id=?
+      SELECT Screening.Screening_id, Movie.Title, Theater.Theater_name,
+      Screening.Screening_startime, Screening.Screening_date
+      
+      FROM Screening
+      INNER JOIN Theater ON Theater.Theater_id = Screening.Theater_id
+      INNER JOIN Movie ON Movie.Movie_id = Screening.Movie_id
+      WHERE Screening.movie_id=?
     `, [movieID]);
 
     return result;
