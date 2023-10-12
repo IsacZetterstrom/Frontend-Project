4d7c0742706dab88d91d33e010658a6dd9fdbd85
lollocodes <louise_johansson.94@hotmail.com>
2023-10-12 12:04:50 +0200

    model to get specific screening on a given date

--- Backend/src/models/screeningModel.js
+++ Backend/src/models/screeningModel.js
@@ -21,4 +21,31 @@ async function getScreenings(movieID) {
   }
 };
 
-export default { getScreenings };
+// Fetch screenings for a specific movie on a given date
+async function getScreeningsByDate(movieID, date) {
+  try {
+    const result = await connection.promise().query(`
+    SELECT 
+      Screening.Screening_id, 
+      Movie.Title, 
+      Theater.Theater_name,
+      Screening.Screening_startime, 
+      Screening.Screening_date
+    FROM Screening
+    INNER JOIN 
+      Theater ON Theater.Theater_id = Screening.Theater_id
+    INNER JOIN 
+      Movie ON Movie.Movie_id = Screening.Movie_id
+    WHERE 
+      Screening.movie_id=? 
+    AND 
+      Screening.Screening_date=?
+    `, [movieID, date]);
+
+    return result[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default { getScreenings, getScreeningsByDate };
