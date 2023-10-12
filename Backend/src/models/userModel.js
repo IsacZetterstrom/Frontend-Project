e14e6e8ccf658e7d6cdec6d07de5130c5f957ea6
SaraJohanssonn <sara.johanss0n@hotmail.com>
2023-10-12 10:46:49 +0200

    added Function to retrieve user bookings

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -5,20 +5,21 @@ async function getUserBookings (user_id) {
   try {
     const query = 
     `
-    SELECT
-        Movie.Title AS MovieTitle,
-        Theater.Theater_name AS TheaterName,
-        Ticket.Seat_id AS Seat,
-        Booking.Ref_num AS ReferenceNumber,
-        Screening.Screening_date AS ScreeningDate,
-        Screening.Screening_startime AS ScreeningStartTime
-    FROM Booking
-    JOIN Ticket ON Booking.Booking_id = Ticket.Booking_id
-    JOIN Screening ON Ticket.Screening_id = Screening.Screening_id
-    JOIN Theater ON Screening.Theater_id = Theater.Theater_id
-    JOIN Movie ON Screening.Movie_id = Movie.Movie_id
-    JOIN Ticket_Type ON Ticket.Ticket_Type_id = Ticket_Type.Ticket_type_id
-    WHERE Booking.User_id = ?;
+    SELECT Movie.Title AS MovieTitle,
+    Theater.Theater_name AS TheaterName,
+    Seat.Number_row AS RowNumber,
+    Seat.Number_seat AS SeatNumber,
+    Booking.Ref_num AS ReferenceNumber,
+    Screening.Screening_date AS ScreeningDate,
+    Screening.Screening_startime AS ScreeningStartTime
+FROM Booking
+JOIN Ticket ON Booking.Booking_id = Ticket.Booking_id
+JOIN Screening ON Ticket.Screening_id = Screening.Screening_id
+JOIN Theater ON Screening.Theater_id = Theater.Theater_id
+JOIN Movie ON Screening.Movie_id = Movie.Movie_id
+JOIN Ticket_Type ON Ticket.Ticket_Type_id = Ticket_Type.Ticket_type_id
+JOIN Seat ON Ticket.Seat_id = Seat.Seat_id
+WHERE Booking.User_id = ?;
     `;
 
     const result = await connection.promise().query(query, [user_id]);    
