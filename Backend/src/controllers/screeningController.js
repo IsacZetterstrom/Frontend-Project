15c7c615ea802a112ef09edb9e37ad79478602f7
lollocodes <louise_johansson.94@hotmail.com>
2023-10-12 12:04:19 +0200

    model to get specific screening on a given date

--- Backend/src/controllers/screeningController.js
+++ Backend/src/controllers/screeningController.js
@@ -1,6 +1,5 @@
 import screeningModel from "../models/screeningModel.js";
 
-
 async function getScreenings(req, res) {
   try {
     const movieID = req.params.movie_id;
@@ -13,5 +12,20 @@ async function getScreenings(req, res) {
   }
 };
 
+async function getScreeningsByDate(req, res) {
+  try {
+    const movieID = req.params.movie_id;
+    const query = req.params.query;
+    const screenings = await screeningModel.getScreeningsByDate(movieID, query);
 
-export default { getScreenings };
+    if (screenings.length === 0) {
+      return res.status(404).json({ error: "No screenings found for the specified date" });
+    }
+
+    res.json(screenings);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export default { getScreenings, getScreeningsByDate };
