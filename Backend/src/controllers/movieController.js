ea0eda8eb4f827e78f0f45a34bc688373fd84114
ledei <holybuzz@live.se>
2023-10-13 15:35:38 +0200

    changed the name of the controller and added filtering model
    Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>

--- Backend/src/controllers/movieController.js
+++ Backend/src/controllers/movieController.js
@@ -21,22 +21,35 @@ async function getOneMovie(req, res) {
   }
 
 /**
- * @author Niklas Nguyen
+ * @author Niklas Nguyen, Isac Zetterström
  * @Description Controller to handle return of currentMovies model
  */
 
-  async function getCurrentMovies(req,res){
+  async function getMovies(req,res){
     try {
-      const movies = await movieModel.currentMovies()
-      if (movies.length > 0) {
-        res.send(movies).status(200)
-      } else {
-        res.status(404).json({ error: "No movies for screening" });
-      }
+      // if(Object.keys(req.query).length === 0){ 
+      //   const movies = await movieModel.currentMovies()
+      //   if (movies.length > 0) {
+      //     res.send(movies).status(200)
+      //   } else {
+      //     res.status(404).json({ error: "No movies for screening" });
+      //   }
+      // }else{
+
+        const { filter, sort, search } = req.query;
+        
+        const movies = await movieModel.filterAllMovies(filter, sort, search)
+        if (movies.length > 0) {
+          res.send(movies).status(200)
+        } else {
+          res.status(404).json({ error: "No movies matches the filters" });
+        }
+        // }
+     
     } catch (error) {
-      console.log(error);
       res.status(500).json({ error: "Internal Server Error" });
+      console.log(error);
     }
   }
 
-export default { getOneMovie,getCurrentMovies };
+export default { getOneMovie,getMovies };
