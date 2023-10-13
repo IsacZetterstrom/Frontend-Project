619565f2b09e2c8e536990c3e2a7d7c59fa2f206
Oliver Andersson <oliver.andersson101@gmail.com>
2023-10-13 13:16:41 +0200

    Model returns seperate bookedSeats and freeSeats array

--- Backend/src/models/screeningModel.js
+++ Backend/src/models/screeningModel.js
@@ -27,6 +27,53 @@ async function getScreenings(movieID) {
   }
 };
 
+// Author: Oliver Andersson
+// Fetch a single screening using screeningID
+async function getScreening(screeningID) {
+  try {
+    
+    const screeningInfo = await connection.promise().query(`
+      SELECT Screening.Screening_id, Theater.Theater_id, Movie.Title, Theater.Theater_name,
+      Screening.Screening_startime, Screening.Screening_date
+      
+      FROM Screening
+      INNER JOIN Theater ON Theater.Theater_id = Screening.Theater_id
+      INNER JOIN Movie ON Movie.Movie_id = Screening.Movie_id
+      WHERE Screening.Screening_id=?
+    `, [screeningID]);
+
+    // If screening is found, fetch seats
+    if(screeningInfo[0][0]) {
+     
+      const bookedSeats = await connection.promise().query(`
+        SELECT Seat.Number_row, Seat.Number_seat
+        FROM Screening
+        INNER JOIN Ticket ON Ticket.Screening_id = Screening.Screening_id
+        INNER JOIN Seat ON Seat.Seat_id = Ticket.Seat_id
+        AND Seat.Theater_id = Screening.Theater_id
+        WHERE Screening.Screening_id=?
+      `, [screeningID]);
+
+      const freeSeats = await connection.promise().query(`
+        SELECT Seat.Number_row, Seat.Number_seat
+        FROM Seat
+        INNER JOIN Screening ON Seat.Theater_id = Screening.Theater_id
+        LEFT JOIN Ticket ON Ticket.Seat_id = Seat.Seat_id AND Ticket.Screening_id = Screening.Screening_id
+        WHERE Screening.Screening_id=? AND Ticket.Seat_id IS NULL;
+      `, [screeningID]);
+
+      screeningInfo[0][0].bookedSeats = bookedSeats[0]
+      screeningInfo[0][0].freeSeats = freeSeats[0]
+    }
+
+    return screeningInfo;
+
+  } catch (error) {
+    console.error('Error in getScreenings model', error);
+    throw error;
+  }
+};
+
 /*
 Author: Louise Johansson
 Description: Model to get specific screening based on date
@@ -57,4 +104,4 @@ async function getScreeningsByDate(movieID, date) {
   }
 }
 
-export default { getScreenings, getScreeningsByDate };
+export default { getScreenings, getScreening, getScreeningsByDate };
