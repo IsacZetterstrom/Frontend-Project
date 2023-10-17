0a54cba3b03b3e74cb752232853f794b446bd733
ledei <holybuzz@live.se>
2023-10-17 16:35:26 +0200

    added a quary for get a booking and refactor codes names and made so create booking return an id
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Louise Johansson <lollocodes@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <Oliver-Andersson19@users.noreply.github.com>
    Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>

--- Backend/src/models/bookingModel.js
+++ Backend/src/models/bookingModel.js
@@ -2,38 +2,60 @@ import connection from "../config/database.js";
 
 /**
  * @Author Niklas Nguyen, Isac Zetterström, Oliver Andersson, Louise Johansson, Oskar Dahlberg
- * @Descriptions model handle bookings
+ * @Descriptions model to get a specific booking
  */
 async function getBooking(bookingId) {
-  const [rows] = await connection.execute(
-    "SELECT * from Booking WHERE Booking.id = ?",
+  const [booking] = await connection.execute(
+    `SELECT
+    Movie.Title AS title,
+    Screening.Screening_startime AS dateAndTime,
+    Booking.Total_price AS priceSum,
+    Booking.Ref_num AS bookingRef,
+    Theater.Theater_name AS saloon,
+    GROUP_CONCAT(  'Rad ',
+    Seat.Number_row,
+    ' Stol ',
+    Seat.Number_seat ORDER BY Seat.Seat_id ASC) AS seats
+    FROM Booking
+    JOIN Ticket ON Ticket.Booking_id = Booking.Booking_id
+    JOIN Seat ON Seat.Seat_id = Ticket.Seat_id
+    JOIN Screening ON Screening.Screening_id = Ticket.Screening_id
+    JOIN Movie ON Movie.Movie_id = Screening.Movie_id
+    JOIN Theater ON Theater.Theater_id = Screening.Theater_id
+    WHERE Booking.Booking_id = ?
+    GROUP BY
+    Movie.Title,
+    Screening.Screening_startime,
+    Booking.Total_price,
+    Booking.Ref_num,
+    Theater.Theater_name`,
     [bookingId]
   );
-  return rows;
+  return booking;
 }
 
 /**
  * @Author Niklas Nguyen, Isac Zetterström, Oliver Andersson, Louise Johansson, Oskar Dahlberg
- * @Descriptions model handle bookings
+ * @Descriptions model to create a booking
  */
 
 async function createBooking(totalPrice, userInfo) {
   //Add total price later
-  if (userInfo) {
+  if (userInfo !== undefined) {
     //Insert if the user is logged in
     const [rows] = await connection.execute(
       "INSERT INTO Booking (User_id,Total_price,Ref_num) VALUES (?,?,(HEX(RANDOM_BYTES(3))))",
-      [userInfo, totalPrice]
+      [userInfo.id, totalPrice]
     );
 
-    return rows;
+    return rows.insertId;
   } else {
     //if not logged in
     const [rows] = await connection.execute(
       "INSERT INTO Booking (Total_price,Ref_num) VALUES (?,(HEX(RANDOM_BYTES(3))))",
       [totalPrice]
     );
-    return rows;
+    return rows.insertId;
   }
 }
 
@@ -42,13 +64,15 @@ async function createBooking(totalPrice, userInfo) {
  * @Description Model to removes specific bookings and tickets
  */
 
-async function deleteBooking(bookingId, userInfo) {
+async function deleteBooking(bookingId, userId) {
   //Get user id from booking,
-  const [bookingUserId] = await connection.execute(
+  const [userData] = await connection.execute(
     "SELECT Booking.User_id FROM Booking WHERE Booking.Booking_id = ?",
     [bookingId]
   );
-  if (bookingUserId[0].User_id !== userInfo[0].User_id)
+
+    if(userData.length === 0) return userData
+  if (userData[0].User_id !== userId)
     throw new Error("You dont have permission to remove this booking");
 
   //Delete tickets based on booking id
