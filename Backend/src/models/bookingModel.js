4a39561fc35f494f100a06c228ee9ffe65d7546e
IsacZetterström <isac.zetterstrom@hotmail.se>
2023-10-17 11:19:06 +0200

    added a model to create booking
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Louise Johansson <lollocodes@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <Oliver-Andersson19@users.noreply.github.com>
    Co-authored-by: Isac Zetterström <113009613+IsacZetterstrom@users.noreply.github.com>

--- Backend/src/models/bookingModel.js
+++ Backend/src/models/bookingModel.js
@@ -1,6 +1,29 @@
 import connection from "../config/database.js";
 
 /**
+ * @Author Niklas Nguyen, Isac Zetterström, Oliver Andersson, Louise Johansson, Oskar Dahlberg
+ * @Descriptions model handle bookings
+ */
+async function createBooking(totalPrice, userInfo) {
+  //Add total price later
+  if (userInfo) {
+    //Insert if the user is logged in
+    const [rows] = await connection.execute(
+      "INSERT INTO Booking (User_id,Total_price,Ref_num) VALUES (?,?,(HEX(RANDOM_BYTES(3))))",
+      [userInfo, totalPrice]
+    );
+    return rows;
+  } else {
+    //if not logged in
+    const [rows] = await connection.execute(
+      "INSERT INTO Booking (Total_price,Ref_num) VALUES (?,(HEX(RANDOM_BYTES(3))))",
+      [totalPrice]
+    );
+    return rows;
+  }
+}
+
+/**
  * @Author Niklas Nguyen
  * @Description Model to removes specific bookings and tickets
  */
@@ -11,10 +34,9 @@ async function deleteBooking(bookingId, userInfo) {
     "SELECT Booking.User_id FROM Booking WHERE Booking.Booking_id = ?",
     [bookingId]
   );
-  if (bookingUserId[0].User_id !== userInfo[0].User_id) {
-    // check if doesnt  match
+  if (bookingUserId[0].User_id !== userInfo[0].User_id)
     throw new Error("You dont have permission to remove this booking");
-  }
+
   //Delete tickets based on booking id
   const [ticketRows] = await connection.execute(
     "DELETE FROM Ticket WHERE Ticket.Booking_id =?",
@@ -28,4 +50,4 @@ async function deleteBooking(bookingId, userInfo) {
   return [ticketRows, bookingRows];
 }
 
-export default { deleteBooking };
+export default { deleteBooking, createBooking };
