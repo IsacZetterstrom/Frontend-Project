6a71607279c589c08bf133698ae3048367ff12b0
ledei <holybuzz@live.se>
2023-10-24 10:24:32 +0200

    fixed the outlet from react-router-dom
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: KernelTestPilot <KernelTestPilot@users.noreply.github.com>
    Co-authored-by: Louise Johansson <lollocodes@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <Oliver-Andersson19@users.noreply.github.com>
    Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,17 @@
-import { useState, useEffect } from 'react'
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  // const [json, setJson] = useState('')
-
-  // useEffect(() => {
-  //   // koppla på routen
-  //   const eventSource = new EventSource('http://localhost:3050/api/movies/screenings/1')
-    
-  //   // Ta emot data från server här
-  //   eventSource.onmessage = event => {
-  //     setJson(JSON.parse(event.data)) // Spara ny data i state
-  //   }
-
-  //   return () => eventSource.close();
-  // }, [])
-  
-  // console.log(json)
-
   return (
-    <div className="App">
-    </div>
-  )
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
