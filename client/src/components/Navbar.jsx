8d812bda3ecd16faed2e97d61db6dfcf84b153b9
ledei <holybuzz@live.se>
2023-10-24 10:26:40 +0200

    we made a test nav
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: KernelTestPilot <KernelTestPilot@users.noreply.github.com>
    Co-authored-by: Louise Johansson <lollocodes@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <Oliver-Andersson19@users.noreply.github.com>
    Co-authored-by: Isac Zetterström <IsacZetterstrom@users.noreply.github.com>

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { pages } from "../router/routes";
+import { NavLink } from "react-router-dom";
+import BootStrapNav from "react-bootstrap/Navbar";
 
 function Navbar() {
+  // function isLoggedIn() {
+  //   if (localStorage.getItem)
+  // }
+
   return (
-    <>Navbar</>
-  )
+    <BootStrapNav>
+      {pages.map(({ label, path, inNav, rightNav }) => {
+        return (
+          inNav &&
+          !rightNav && (
+            <NavLink key={path} className="nav-link" to={path}>
+              {label}
+            </NavLink>
+          )
+        );
+      })}
+      {<h1>JÄTTE FIN BILD</h1>}
+      {pages.map(({ label, path, inNav, rightNav }) => {
+        return (
+          inNav &&
+          rightNav && (
+            <NavLink key={path} className="nav-link" to={path}>
+              {label}
+            </NavLink>
+          )
+        );
+      })}
+    </BootStrapNav>
+  );
 }
 
-export default Navbar
+export default Navbar;
