beec608274545b32ab78847a3d5e01c1f688294f
KernelTestPilot <47748828+KernelTestPilot@users.noreply.github.com>
2023-10-24 17:44:56 +0200

    render out links to movies

--- client/src/pages/LibraryPage.jsx
+++ client/src/pages/LibraryPage.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 function LibraryPage() {
+//http://localhost:3050/api/movies?filter=&sort=&search=
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+        const response = await fetch('/api/movies?filter=&sort=&search=');
+        if (response.ok) {
+          const data = await response.json();
+          setData(data);
+          console.log(data)
+        } else {
+          console.error('Failed to fetch data');
+        }
+    })();
+
+}, [])
+
+
+
   return (
-    <>LibraryPage</>
+    <>
+    <h1>Libarypage</h1>
+    <Container>
+      <Row>
+        {data.map((movie) => (
+          <Col sm={4} key={movie.id}>
+                 <Link to={`/film/${movie.Movie_id}`}>
+                        {movie.Title} 
+                 </Link>
+  
+          </Col>
+        ))}
+        </Row>
+            </Container>
+        </>
   )
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/events': {
-        target: `http://localhost:3050`,
+      '/api': {
+        target: 'http://localhost:3050',
         changeOrigin: true,
-        timeout: 0,
-        secure: false,
-      },
-    },
+        secure: false
+      }
+    }
+
   }
 });
