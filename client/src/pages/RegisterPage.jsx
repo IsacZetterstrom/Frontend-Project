a2c870b45a01f918c8fd71cce0837ad20b221bcf
ledei <holybuzz@live.se>
2023-10-25 12:39:06 +0200

    added all the componets for register page
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useFormDefaults } from "../hooks/useFormDefaults";
+import RegisterForm from "../components/Forms/RegisterForm";
+import fetchService from "../service/FetchService";
+import { Container, Row } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 function RegisterPage() {
+  const { defaults, formData, setFormData } = useFormDefaults();
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.email === "" || formData.password === "") return false;
+    const res = await fetchService.fetchRes("auth/register", "POST", formData);
+    if (res.status >= 400) {
+      setMsg("Blev fel vid registreringen");
+      return false;
+    } else {
+      setMsg("Du kommer nu bli dirigerad för att logga in");
+      setTimeout(() => navigate("/logga-in"), 2000);
+    }
+  };
+
   return (
-    <>RegisterPage</>
-  )
+    <Container fluid className="form-wrapper">
+      <Row>
+        <h1>Bli medlem</h1>
+        <RegisterForm {...{ defaults, formData, handleSubmit, setFormData }} />
+        {msg !== null && <p>{msg}</p>}
+        <p>
+          Har du redan ett konto?<Link to={"/logga-in"}>Logga in här!</Link>
+        </p>
+      </Row>
+    </Container>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
