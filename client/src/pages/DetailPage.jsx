21b6647ceda617b12e2c05a915e7025edfa95bd1
lollocodes <louise_johansson.94@hotmail.com>
2023-10-25 09:23:31 +0200

    added mockdata

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import TrailerComponent from '../components/DetailPage/TrailerComponent'
+import MovieDesc from '../components/DetailPage/MovieDesc'
 
 function DetailPage() {
+  const Movie = {
+      status: 200,
+      message: "Movie found",
+      movie: {
+        Movie_id: 1,
+        Title: "Jack Reacher: Never Go Back",
+        Release_date: "2016-10-19T00:00:00.000Z",
+        Genre: "Action",
+        Rating: 5.945,
+        Director_name: "Edward Zwick",
+        About: "When Major Susan Turner is arrested for treason, ex-investigator Jack Reacher undertakes the challenging task to prove her innocence and ends up exposing a shocking conspiracy.",
+        Runtime: 118,
+        Poster: "https://image.tmdb.org/t/p/original/cOg3UT2NYWHZxp41vpxAnVCOC4M.jpg",
+        Image: "https://image.tmdb.org/t/p/original/ww1eIoywghjoMzRLRIcbJLuKnJH.jpg",
+        Lang: "EN",
+        Trailer: "https://www.youtube.com/watch?v=aRwrdbcAh2s",
+        Actor_names: "Tom Cruise"
+      }
+  }
+
   return (
-    <>DetailPage</>
+    <>
+      <div className="detailPageContainer">
+        <TrailerComponent movie={Movie.movie} />
+        <MovieDesc movie={Movie.movie}/>
+      </div>
+    </>
   )
 }
 
