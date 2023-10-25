567b1f04035c50a6307ea5e25aefc0789d568dcf
lollocodes <louise_johansson.94@hotmail.com>
2023-10-25 11:30:45 +0200

    useFetchData for fetching data instead of mockdata

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,36 +1,26 @@
-import React from 'react'
-import TrailerComponent from '../components/DetailPage/TrailerComponent'
-import MovieDesc from '../components/DetailPage/MovieDesc'
+import React, { useEffect, useState } from 'react';
+import TrailerComponent from '../components/DetailPage/TrailerComponent';
+import MovieDesc from '../components/DetailPage/MovieDesc';
+import { useParams } from 'react-router-dom';
+import useFetchData from '../hooks/useFetchData';
 
 function DetailPage() {
-  const Movie = {
-      status: 200,
-      message: "Movie found",
-      movie: {
-        Movie_id: 1,
-        Title: "Jack Reacher: Never Go Back",
-        Release_date: "2016-10-19T00:00:00.000Z",
-        Genre: "Action",
-        Rating: 5.945,
-        Director_name: "Edward Zwick",
-        About: "When Major Susan Turner is arrested for treason, ex-investigator Jack Reacher undertakes the challenging task to prove her innocence and ends up exposing a shocking conspiracy.",
-        Runtime: 118,
-        Poster: "https://image.tmdb.org/t/p/original/cOg3UT2NYWHZxp41vpxAnVCOC4M.jpg",
-        Image: "https://image.tmdb.org/t/p/original/ww1eIoywghjoMzRLRIcbJLuKnJH.jpg",
-        Lang: "EN",
-        Trailer: "https://www.youtube.com/watch?v=aRwrdbcAh2s",
-        Actor_names: "Tom Cruise"
-      }
-  }
+  const { movieId } = useParams();
+  const { loading, err, data } = useFetchData(`/api/movies/${movieId}`);
 
   return (
     <>
       <div className="detailPageContainer">
-        <TrailerComponent movie={Movie.movie} />
-        <MovieDesc movie={Movie.movie}/>
-      </div>
+      {err && <p>404 could not be found</p>}
+      {loading ? <p>loading...</p> : (
+        <>
+          <TrailerComponent movie={data?.movie} />
+          <MovieDesc movie={data?.movie} />
+        </>
+      )}
+    </div>
     </>
-  )
+  );
 }
 
-export default DetailPage
+export default DetailPage;
