eaf4dbb22ca2a4cbc7858139f03419a0b2aa89ab
ledei <holybuzz@live.se>
2023-10-25 15:30:48 +0200

    added bootstrap classname and fixed typo Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>

--- client/src/components/Forms/RegisterForm.jsx
+++ client/src/components/Forms/RegisterForm.jsx
@@ -4,32 +4,38 @@ import FormBtns from "./FormBtns";
 
 export default function RegisterForm({ defaults, formData, handleSubmit, setFormData }) {
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="p-0" onSubmit={handleSubmit}>
       <Container className="mt-3">
-        <label>E-Post</label>
+        <Container className="d-flex justify-content-center align-items-center p-0">
+          <label className="p-0 text-nowrap">E-Post</label> <Container className="navline d-md-block" />
+        </Container>
         <input {...defaults("email", "", { minLength: 8, type: "email" })} />
       </Container>
       <Container className="mt-3">
-        <label>Välj ett lössenord</label>
+        <Container className="d-flex justify-content-center align-items-center p-0">
+          <label className="p-0 text-nowrap">Lösenord</label> <Container className="navline d-md-block" />
+        </Container>
         <input
           {...defaults(
             "password",
             "",
             { minLength: 8, type: "password" },
             (val) => /\d/.test(val) && /[A-Z]/.test(val) && /[a-z]/.test(val),
-            "Lössenordet måste innehålla minst ett stort bokstav,ett litet och ett siffra!"
+            "Lösenordet måste innehålla minst ett stort bokstav,ett litet och ett siffra!"
           )}
         />
       </Container>
       <Container className="mt-3">
-        <label>Bekräfta lössenord</label>
+        <Container className="d-flex justify-content-center align-items-center p-0">
+          <label className="p-0 text-nowrap">Bekräfta lösenord</label> <Container className="navline d-md-block" />
+        </Container>
         <input
           {...defaults(
             "confirmPassword",
             "",
             { minLength: 8, type: "password" },
             (val) => val === formData.password,
-            "Du måste ha matchande lössenord!"
+            "Du måste ha matchande lösenord!"
           )}
         />
       </Container>
