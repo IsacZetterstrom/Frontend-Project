f5cbf28f1e413ad35d582c13db5695882a831422
Oliver Andersson <oliver.andersson101@gmail.com>
2023-10-25 10:35:46 +0200

    eventsource and handle seat click

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,24 +1,56 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import SeatPicker from "../components/SeatPicker";
 
 function BookingPage() {
-  const [json, setJson] = useState("");
+  
+  const { screeningId } = useParams();
 
+  const [screeningData, setScreeningData] = useState({});
+  
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [maxSeats, setMaxSeats] = useState(3);
+
+
+
+  // Flytta ut denna useEffect?
   useEffect(() => {
-    // koppla på routen
-    const eventSource = new EventSource("http://localhost:3050/api/movies/screenings/1");
+    const eventSource = new EventSource("http://localhost:3050/api/movies/screenings/" + screeningId);
 
-    // Ta emot data från server här
     eventSource.onmessage = (event) => {
-      setJson(JSON.parse(event.data)); // Spara ny data i state
+      setScreeningData(JSON.parse(event.data));
     };
 
     return () => eventSource.close();
   }, []);
 
-  // console.log(json);
 
-  return <>BookingPage</>;
+
+  function handleSeatClick(seat) {
+    if(selectedSeats.includes(seat)) {
+      setSelectedSeats(selectedSeats.filter((x) => x !== seat))
+    } else {
+
+      if (selectedSeats.length >= maxSeats) {
+        // Remove the first item from selectedSeats before adding a new one
+        const updatedSeats = selectedSeats.slice(1);
+        setSelectedSeats([...updatedSeats, seat]);
+      } else {
+        setSelectedSeats([...selectedSeats, seat]);
+      }
+    }
+  }
+
+  console.log(selectedSeats)
+
+  return <>
+    <SeatPicker
+      screeningData={screeningData}
+      handleSeatClick={handleSeatClick}
+      selectedSeats={selectedSeats}
+    />
+  </>;
 }
 
 export default BookingPage;
