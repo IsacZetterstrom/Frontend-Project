40d6e1472ff3465a50c485476729b087c13b4c01
lollocodes <louise_johansson.94@hotmail.com>
2023-10-25 11:29:07 +0200

    changed proxy to /api

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/events': {
-        target: `http://localhost:3050`,
+      "/api": {
+        target: "http://localhost:3050/",
         changeOrigin: true,
-        timeout: 0,
         secure: false,
       },
     },
-  }
+  },
 });
