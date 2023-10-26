cce7a5f3fe885f570908989c2c5506f649a6b15e
lollocodes <louise_johansson.94@hotmail.com>
2023-10-26 15:04:31 +0200

    merged trailercomponent with screeningcomponent

--- client/src/components/DetailPage/MovieDesc.jsx
+++ client/src/components/DetailPage/MovieDesc.jsx
@@ -10,7 +10,7 @@ function MovieDesc({ movie }) {
   return (
     <Container className='movieDescContainer mt-2'>
         <Row>
-            <Col xs={5} sm={6} md={6}>
+            <Col xs={5} sm={6} md={6} className='d-lg-none'>
                 <Image src={movie.Poster} alt={movie.Title} fluid className="max-200 max-300 poster" />
             </Col>
             <Col xs={7} sm={6} md={6} lg={12}>
