825aca56a78c494b5a992b7b4e39eb4828039364
lollocodes <louise_johansson.94@hotmail.com>
2023-10-26 09:56:31 +0200

    small changes to breakpoints

--- client/src/components/DetailPage/MovieDesc.jsx
+++ client/src/components/DetailPage/MovieDesc.jsx
@@ -5,11 +5,11 @@ function MovieDesc({ movie }) {
   return (
     <Container className='movieDescContainer mt-2'>
         <Row>
-            <Col xs={6} md={6} className='d-lg-none'>
-                <Image src={movie.Poster} alt={movie.Title} fluid className="max-200 max-300" />
+            <Col xs={5} sm={6} md={6}>
+                <Image src={movie.Poster} alt={movie.Title} fluid className="max-200 max-300 poster" />
             </Col>
-            <Col xs={6} md={6} lg={12}>
-                <h2 className='movieTitle'>{movie.Title}</h2>
+            <Col xs={7} sm={6} md={6} lg={12}>
+                <h1 className='movieTitle'>{movie.Title}</h1>
                 <h2>Genre</h2>
                 <p>{movie.Genre}</p>
                 <h2>Beskrivning</h2>
