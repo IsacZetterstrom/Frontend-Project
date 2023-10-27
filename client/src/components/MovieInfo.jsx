0e8436c7e0a666884d837ac5cdcd7e4616b4fca5
SaraJohanssonn <sara.johanss0n@hotmail.com>
2023-10-27 12:32:21 +0200

    created Movieinfo component

---
+++ client/src/components/MovieInfo.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import '../styling/components/_movieInfo.scss'
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+/**
+ * @author Sara Johansson
+ * @description lalala
+ */
+
+
+function MovieInfo() {
+ 
+return (
+<Container className='movieContainer mt-4 flex-column'>
+        <Row>
+            <Col xs={6} md={6} className='moviePoster d-lg-none'>
+            <Image src='' alt='' fluid className="max-200 max-300" /><p>BILD</p>
+            </Col>
+            <Col>
+            <h2 className='movieTitle mb-2'>{movie.About}</h2>
+                <p className='text-sm mb-0'>Fredag, 24 oktober</p>
+                <p className='text-sm mb-0'>Kl. 17.00 - 18.45</p>
+                <p className='text-sm mb-0'></p>
+                <p className='text-sm mb-0'>Salong 7</p>
+            </Col>
+        </Row>
+    </Container>
+);
+}
+
+export default MovieInfo;
