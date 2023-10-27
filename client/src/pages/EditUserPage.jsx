13076a16f090a6ff33110174fc6d85dd35470d36
ledei <holybuzz@live.se>
2023-10-27 14:43:35 +0200

    created the page to hold edit user form
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>

--- client/src/pages/EditUserPage.jsx
+++ client/src/pages/EditUserPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useFormDefaults } from "../hooks/useFormDefaults";
+import EditUserForm from "../components/Forms/EditUserForm";
+import { Container, Row } from "react-bootstrap";
+import useFetchData from "../hooks/useFetchData";
+import fetchService from "../service/FetchService";
 
-function EditUserPage() {
+function EditUserPage({ setEditUser }) {
+  const { defaults, formData, setFormData } = useFormDefaults();
+  const { loading, err, data } = useFetchData(`/profile/user`);
+  const [msg, setMsg] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.email === undefined) formData.email = null;
+    if (formData.firstname === undefined) formData.firstname = null;
+    if (formData.lastname === undefined) formData.lastname = null;
+    if (formData.phone === undefined) formData.phone = null;
+
+    const res = await fetchService.fetchRes("/profile/user", "PATCH", formData);
+    if (res.status >= 400) {
+      setMsg("Det blev fel vid uppdateringen");
+      return false;
+    } else {
+      setMsg("Din uppgifter har uppdaterats");
+      setTimeout(() => setEditUser((val) => !val), 2000);
+    }
+  };
+
   return (
-    <>EditUserPage</>
-  )
+    <Container className="form-wrapper">
+      <Row>
+        <h1 className="p-0 text-nowrap fs-1 mt-5 mb-5 line pb-2">Redigera uppgifter</h1>
+        {(loading && <p className="text-center">loading...</p>) || (err && <p className="text-center">An Error occured</p>) || (
+          <EditUserForm {...{ defaults, handleSubmit, setFormData, userData: data }} />
+        )}
+        {msg !== "" && <p className="text-center">{msg}</p>}
+      </Row>
+    </Container>
+  );
 }
 
-export default EditUserPage
+export default EditUserPage;
