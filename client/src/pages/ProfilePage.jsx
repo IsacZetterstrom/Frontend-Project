95301da88ad60f283d4fe877332114ece1530082
IsacZetterström <isac.zetterstrom@hotmail.se>
2023-10-27 10:52:32 +0200

    added active, and expired bookings components to render

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import useFetchData from "../hooks/useFetchData";
+import { Container, Row, Col } from "react-bootstrap";
+import ActiveBookings from "../components/ProfilePage/ActiveBookings";
+import ExpiredBookings from "../components/ProfilePage/ExpiredBookings";
+import LoadingGif from "../components/misc/loadingGif";
 
 function ProfilePage() {
+  const { loading, err, data } = useFetchData("profile/user/bookings");
+  // console.log(data);
   return (
-    <>ProfilePage</>
-  )
+    <Container className="profile-container">
+      {loading ? (
+        <LoadingGif />
+      ) : (
+        <>
+          <h1>Min Sida</h1>
+          <ActiveBookings activeBookings={data?.active} />
+          <ExpiredBookings expiredBookings={data?.expired} />
+        </>
+      )}
+    </Container>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
