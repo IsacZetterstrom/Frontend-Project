98d4572eaec4b05fb2bd7c37420646ff4d247e91
IsacZetterström <isac.zetterstrom@hotmail.se>
2023-10-27 10:53:49 +0200

    simple functions to get first, and last char in a string.

---
+++ client/src/utils/stringFormatter.js
@@ -0,0 +1,13 @@
+function getFirstLetter(string) {
+  return = str.charAt(0)
+}
+function getLastLetter(string) {
+  return = str.charAt(str.length - 1)
+}
+
+const stringFormatter = {
+  getFirstLetter,
+  getLastLetter,
+};
+
+export default stringFormatter;
