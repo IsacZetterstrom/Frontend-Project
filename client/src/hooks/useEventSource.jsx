ce88d45e104416885efeac4f91e4e91ac4bf958a
Oliver Andersson <oliver.andersson101@gmail.com>
2023-10-27 16:14:43 +0200

    put eventSource in custom hook

---
+++ client/src/hooks/useEventSource.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function useEventSource(url) {
+    const [err, setErr] = useState(false);
+    const [screeningData, setScreeningData] = useState();
+    useEffect(() => {
+
+        const eventSource = new EventSource(url);
+        eventSource.onmessage = (event) => {
+            setScreeningData(JSON.parse(event.data));
+        };
+        
+        eventSource.onerror = (e) => {
+            setErr(true);
+        };
+        
+        return () => eventSource.close();
+       
+    }, [url]);
+    return { err, screeningData };
+  }
+  export default useEventSource;
+  
