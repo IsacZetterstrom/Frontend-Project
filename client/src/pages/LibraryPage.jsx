adcb7fbe0047bcd12d469801fb35d303e6b8cd9e
ledei <holybuzz@live.se>
2023-10-30 16:05:59 +0100

    added the hero card for libraryPage
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>

--- client/src/pages/LibraryPage.jsx
+++ client/src/pages/LibraryPage.jsx
@@ -5,6 +5,8 @@ import GlobalMovieCard from "../components/GlobalMovieCard";
 import useQuaryMovie from "../hooks/useQuaryMovie";
 import { useFormDefaults } from "../hooks/useFormDefaults";
 import SearchFilterSortQuary from "../components/LibaryPageComp/SearchFilterSortQuary";
+import Hero from "../components/LandingPageComp/Hero";
+import useFetchData from "../hooks/useFetchData";
 
 /**
  * @author Oskar dahlberg
@@ -13,12 +15,22 @@ import SearchFilterSortQuary from "../components/LibaryPageComp/SearchFilterSort
 function LibraryPage() {
   const { defaults, formData: quary } = useFormDefaults();
   const { loading, err, data } = useQuaryMovie(quary.search, quary.sort, quary.filter);
+  const { loading: isLoading, err: error, data: heroData } = useFetchData("api/movies/1");
 
   return (
     <>
-      <Container className="mt-5 library-page">
+      <Container className="mt-5 d-flex flex-column align-items-center justify-content-center library-page">
+        <Row className="justify-content-center p0 m-0 w-75 hero-container">
+          {(isLoading && <p>laddar...</p>) || (error && <p>Error</p>) || (
+            <>
+              <h1 className="line pb-2">Populär just nu</h1>
+              <Hero {...{ heroData }} />
+              <h1 className="text-center mb-5 p-1 hero-title">{heroData.movie.Title}</h1>
+            </>
+          )}
+        </Row>
         <SearchFilterSortQuary {...{ defaults }} />
-        <Row>
+        <Row className="w-75">
           <h1 className="line pb-2">På bio nu</h1>
           {(loading && <p>laddar....</p>) ||
             (err && <p>Hittade inte filmerna</p>) ||
