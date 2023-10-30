8979005de3551cca973839df69f9695f7af84685
lollocodes <louise_johansson.94@hotmail.com>
2023-10-30 16:00:49 +0100

    added form for booking a screening

--- client/src/components/BookingPage/BookingForm.jsx
+++ client/src/components/BookingPage/BookingForm.jsx
@@ -1,35 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useFormDefaults } from '../../hooks/useFormDefaults';
-import { useOutletContext } from 'react-router-dom';
-import { Container, Row } from "react-bootstrap";
-import LoginForm from "../Forms/LoginForm";
+import { useParams } from "react-router-dom";
+import { Container, Row, Form, Col } from "react-bootstrap";
+import FormBtns from "../Forms/FormBtns";
+import useFetchData from '../../hooks/useFetchData';
+import fetchService from "../../service/FetchService";
 
-function BookingForm() {
+function BookingForm({ bookingInfo }) {
     const { defaults, formData, setFormData } = useFormDefaults();
-    const [isLoggedIn, setIsLoggedIn] = useOutletContext();
+    const { loading, err, data } = useFetchData("/profile/user");
+    const { movieId, screeningId } = useParams();
+    const [confirmationData, setConfirmationData] = useState(null);
+    const [error, setError] = useState(null); // State for error message
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+      e.preventDefault();
+  
+      // Check if email is filled in
+      if (!formData.email) return false;
+  
+      // Create the data object to send in the POST request
+      const data = {
+        tickets: bookingInfo.tickets,
+        email: formData.email, 
       };
-
-  return (
-    <Container className="form-wrapper">
-    <Row>
-      <h1 className="p-0 text-nowrap mt-5 mb-5 pb-2 line">Logga in</h1>
-      <Form className="p-0" onSubmit={handleSubmit}>
-      <Col className="mt-3">
-        <label className="p-0 text-nowrap line d-block">E-Post</label>
-        <input {...defaults("email", "", { minLength: 8, type: "email" })} />
-      </Col>
-      <Col className="mt-3">
-        <label className="p-0 text-nowrap d-block line">Lösenord</label>
-        <input {...defaults("password", "", { type: "password" })} />
-      </Col>
-      <FormBtns {...{ submitBtn: "Logga in", cancelBtn: "Avbryt", showCancelBtn: false, setFormData }} />
-    </Form>
-    </Row>
-  </Container>
-  )
+  
+      // Send the post request to the backend
+      try {
+        const response = await fetchService.fetchRes(
+          `/api/movies/${movieId}/screenings/${screeningId}/booking`,
+          'POST',
+          data
+        );
+  
+        if (response.ok) {
+            const responseData = await response.json(); 
+            setConfirmationData(responseData);
+          } else {
+            const errorData = await response.json();
+            setError(errorData.error); 
+          }
+      } catch (error) {
+        console.error('Error:', error);
+        setError("An error occurred during booking.");
+      }
+    };
+  
+    // Ensure data is fetched before rendering the form
+    useEffect(() => {
+      if (data && data.email) {
+        setFormData({ email: data.email });
+      }
+    }, [data, setFormData]);
+  
+    return (
+      <Container className="form-wrapper">
+        <Row>
+          <h1 className="p-0 text-nowrap mt-5 mb-5 pb-2 line">Bekräfta din bokning</h1>
+          {error && <p className="text-danger">{error}</p>}
+          <Form className="p-0" onSubmit={handleSubmit}>
+            <Col className="mt-3">
+              <label className="p-0 text-nowrap line d-block">E-Post</label>
+              <input
+                {...defaults("email", formData?.email || '', { minLength: 8, type: "email" })}
+                value={formData?.email || ''}
+              />
+            </Col>
+            <FormBtns
+              {...{ submitBtn: "Boka", cancelBtn: "Avbryt", showCancelBtn: true, setFormData }}
+            />
+          </Form>
+        </Row>
+      </Container>
+    );
 }
 
-export default BookingForm
+export default BookingForm;
