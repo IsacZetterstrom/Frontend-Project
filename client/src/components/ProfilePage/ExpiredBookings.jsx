5c4205357c4800431e46f39e35cdf38855bad6b0
IsacZetterström <isac.zetterstrom@hotmail.se>
2023-10-30 10:01:31 +0100

    added comments and length on card

--- client/src/components/ProfilePage/ExpiredBookings.jsx
+++ client/src/components/ProfilePage/ExpiredBookings.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Container, Row, Col, Button, Image } from "react-bootstrap";
-import dateUtils from "../../utils/dateUtils.js";
+import dateUtils, { getMovieEndTime } from "../../utils/dateUtils.js";
+
+/**
+ * @author Isac Zetterström
+ * @description renders cards for expired bookings
+ */
 
 function expiredBookings({ expiredBookings }) {
   return (
     <>
-      <h7>Bokningshistorik</h7>
+      <h6 className="mb-2">Bokningshistorik</h6>
       <Row>
         <Container className=" d-flex flex-column align-items-center">
           {expiredBookings.map((booking) => {
@@ -27,13 +32,21 @@ function expiredBookings({ expiredBookings }) {
                   <p className="mb-0 mx-2">
                     {dateUtils.getDateWithDay(booking.screeningStartTime)}
                   </p>
-                  <p className="mb-0 mx-2">{booking.screeningStartTime}</p>
+                  <p className="mb-0 mx-2">
+                    {getMovieEndTime(
+                      booking.screeningStartTime,
+                      booking.runTime
+                    )}
+                  </p>
                   <p className="mb-0 mx-2">{`${booking.theaterName}, Plats-Stol: ${booking.seats}`}</p>
                   <p className="mb-1 mx-2">{`Pris: ${booking.priceSum} Kr`}</p>
                   <Col className="d-flex justify-content-between">
                     <span className="ref-number mx-2">
                       Bokningsnummer: {booking.referenceNumber}
                     </span>
+                    <Button size="sm" className="cancel-booking mx-2 mb-1">
+                      Avboka
+                    </Button>
                   </Col>
                 </Col>
               </Col>
