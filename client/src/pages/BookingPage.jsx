953de26a7891da5aca908bde59d168743e51b306
ledei <holybuzz@live.se>
2023-10-31 14:43:10 +0100

    fixed how it is in dev branch and added movie info
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -3,10 +3,11 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import SeatPicker from "../components/SeatPicker";
 import TicketSelector from "../components/TicketSelector";
-import MovieInfo from "../components/BookingPage/MovieInfo";
-import { Container, Col, Row } from "react-bootstrap";
-import '../styling/components/_bookingPage.scss';
-import'../styling/components/_movieInfo.scss';
+import { Col, Container, Row } from "react-bootstrap";
+import "../styling/components/_bookingPage.scss";
+import PriceSummary from "../components/PriceSummary";
+import useEventSource from "../hooks/useEventSource";
+import MovieInfo from "../components/MovieInfo";
 
 /**
  * @author Oliver Andersson
@@ -14,41 +15,22 @@ import'../styling/components/_movieInfo.scss';
  */
 
 function BookingPage() {
-  
   const { screeningId } = useParams();
-
-  const [screeningData, setScreeningData] = useState({});
-  
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [maxSeats, setMaxSeats] = useState(2);
+  const { err, screeningData } = useEventSource("http://localhost:3050/api/movies/screenings/" + screeningId);
 
-
-  // Keys are the Ticket_Type_id and the values are how many tickets are chosen for that ticket type 
+  // Keys are the Ticket_Type_id and the values are how many tickets are chosen for that ticket type
   const [tickets, setTickets] = useState({
     1: 0,
     2: 0,
-    3: 2
+    3: 2,
   });
 
-
-
-  useEffect(() => {
-    const eventSource = new EventSource("http://localhost:3050/api/movies/screenings/" + screeningId);
-
-    eventSource.onmessage = (event) => {
-      setScreeningData(JSON.parse(event.data));
-    };
-
-    return () => eventSource.close();
-  }, []);
-
-
-
   function addOneSeat(seat) {
-    if(selectedSeats.includes(seat)) {
-      setSelectedSeats(selectedSeats.filter((x) => x !== seat))
+    if (selectedSeats.includes(seat)) {
+      setSelectedSeats(selectedSeats.filter((x) => x !== seat));
     } else {
-
       if (selectedSeats.length >= maxSeats) {
         const updatedSeats = selectedSeats.slice(1);
         setSelectedSeats([...updatedSeats, seat]);
@@ -62,60 +44,72 @@ function BookingPage() {
     setSelectedSeats(seats);
   }
 
+  // Runs when + or - in ticketSelector gets pressed
   function handleTicketChange(action, type) {
-    let newTickets;
+    let newTickets = tickets;
     let ticketCount = 0;
 
-    if(action == "+") {
-      newTickets = {...tickets, [type]: tickets[type] + 1}
+    if (action == "+") {
+      newTickets = { ...tickets, [type]: tickets[type] + 1 };
     } else if (action === "-" && tickets[type] > 0) {
-      newTickets = {...tickets, [type]: tickets[type] - 1}
+      newTickets = { ...tickets, [type]: tickets[type] - 1 };
     }
 
     for (const [key, value] of Object.entries(newTickets)) {
       ticketCount += value;
     }
 
-    setTickets(newTickets)
-    setMaxSeats(ticketCount)
+    setTickets(newTickets);
+    setMaxSeats(ticketCount);
+    setSelectedSeats([]);
   }
 
+  // Runs when "book" button gets pressed
+  function handleBookingClick() {
+    const data = {
+      tickets: [],
+    };
+
+    // Put ticket and seat data into correct structure for post request
+    for (const [key, value] of Object.entries(tickets)) {
+      for (let i = 0; i < value; i++) {
+        if (selectedSeats[i] === undefined) return;
+
+        data.tickets.push({
+          Screening_id: screeningId,
+          Ticket_Type_id: key,
+          Seat_id: selectedSeats[i].Seat_id,
+        });
+      }
+    }
+
+    console.log(data);
+  }
 
   return (
-    <Container fluid className="booking-page-wrapper p-4">
-      <Row>
-       <Col sm={6} className="lala"> <Row>
-       <MovieInfo screeningData={screeningData}/>
-       </Row>
-       </Col>
-     
-       <Col sm={6}>
-      <h5>Välj antal biljetter</h5>
+    <Container fluid className="booking-page-wrapper w-100 p-0 m-0">
+      <Row className="p-4 m-0">
+        <Col sm={6} className="movie-col">
+          <Row>{(err && <p>err</p>) || <MovieInfo {...{ screeningData }} />}</Row>
+        </Col>
 
-      <TicketSelector
-        tickets={tickets}
-        handleTicketChange={handleTicketChange}
-      />
+        <Col sm={6}>
+          <h5 className="line pb-1">Välj antal biljetter</h5>
 
-      <h5>Välj platser</h5>
+          <TicketSelector {...{ tickets, handleTicketChange }} />
 
-      <SeatPicker
-        screeningData={screeningData}
-        addOneSeat={addOneSeat}
-        addSeveralSeats={addSeveralSeats}
-        selectedSeats={selectedSeats}
-        maxSeats={maxSeats}
-      />
-      </Col>
+          <h5 className="line pb-1">Välj platser</h5>
+
+          {(err && <p>err</p>) || <SeatPicker {...{ screeningData, addOneSeat, addSeveralSeats, selectedSeats, maxSeats }} />}
+        </Col>
+      </Row>
+      <Row className="m-0">
+        <Col className="p-0">
+          <PriceSummary {...{ handleBookingClick, tickets }} />
+        </Col>
       </Row>
     </Container>
-    
-
-
-
-
-
-  )
+  );
 }
 
 export default BookingPage;
