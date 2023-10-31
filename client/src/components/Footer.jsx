4c22fd3f608b00ea595adb00d408a7789f4e9350
ledei <holybuzz@live.se>
2023-10-31 14:41:44 +0100

    added css line
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { BsInstagram, BsLinkedin, BsTwitter, BsTelephone } from 'react-icons/bs';
-import '../styling/components/_footer.scss';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { BsInstagram, BsLinkedin, BsTwitter, BsTelephone } from "react-icons/bs";
+import "../styling/components/_footer.scss";
 
 /**
  * @author Sara Johansson
@@ -11,30 +11,30 @@ import '../styling/components/_footer.scss';
 function Footer() {
   return (
     <Container fluid className="custom-footer">
-      <Row className="icon-row text-center">
-        <Col xs={1} className="icon-col"> 
+      <Row className="icon-row text-center line-center">
+        <Col xs={1} className="icon-col">
           <a href="https://www.twitter.com/" className="icon fs-3">
             <BsTwitter />
           </a>
         </Col>
-        <Col xs={1} className="icon-col"> 
+        <Col xs={1} className="icon-col">
           <a href="https://www.linkedin.com/" className="icon fs-3">
             <BsLinkedin />
           </a>
         </Col>
-        <Col xs={1} className="icon-col"> 
+        <Col xs={1} className="icon-col">
           <a href="https://www.instagram.com/" className="icon fs-3">
             <BsInstagram />
           </a>
         </Col>
       </Row>
       <Row className="justify-content-center text-center mt-4">
-        <Col md={12} className="phone-col"> 
+        <Col md={12} className="phone-col">
           <a href="tel:+1234567891" className="icon">
             <BsTelephone /> 12-34 56 78 91
           </a>
         </Col>
-        <Col md={12} className='footer-adress'>
+        <Col md={12} className="footer-adress">
           <a>Springavägen 420, 133 37 Rymden</a>
         </Col>
       </Row>
