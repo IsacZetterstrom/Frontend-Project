3345acd5a18c31f9ac135828ce78f27cde80d11b
IsacZetterström <isac.zetterstrom@hotmail.se>
2023-10-31 11:28:25 +0100

    Fixed buttons etc for edit user and unbook bookings

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -5,6 +5,7 @@ import ActiveBookings from "../components/ProfilePage/ActiveBookings";
 import ExpiredBookings from "../components/ProfilePage/ExpiredBookings";
 import LoadingGif from "../components/misc/loadingGif";
 import UserInfoCard from "../components/ProfilePage/UserInfoCard";
+import EditUserPage from "../pages/EditUserPage";
 
 /**
  * @author Isac Zetterström
@@ -12,36 +13,41 @@ import UserInfoCard from "../components/ProfilePage/UserInfoCard";
  */
 
 function ProfilePage() {
-  const { loading, err, data } = useFetchData("profile/user/bookings");
+  const [update, setUpDate] = useState(0);
+  const { loading, err, data } = useFetchData("profile/user/bookings", update);
   const [editUser, setEditUser] = useState(false);
-  const toggleEditUser = () => {
-    setEditUser((editUser) => !editUser);
-  };
 
+  function toggle() {
+    setEditUser((editUser) => !editUser);
+  }
   return (
-    <Container className="profile-container">
-      {err && <p>Fel vid hämtning av profilsidan har inträffat</p>}
-      {loading ? (
-        <LoadingGif />
-      ) : (
-        <>
-          <Row>
-            <Col className="offset-sm-2 offset-md-3 offset-lg-3">
-              <h1 className="line pb-1 my-3">Min Sida</h1>
-            </Col>
-          </Row>
-          <Row className="d-flex flex-column flex-lg-row-reverse justify-content-lg-between">
-            <Col lg={9}>
-              <ActiveBookings activeBookings={data?.active} />
-              <ExpiredBookings expiredBookings={data?.expired} />
-            </Col>
-            <Col lg={3}>
-              {!editUser && <UserInfoCard {...{ setEditUser }} />}
-            </Col>
-          </Row>
-        </>
-      )}
-    </Container>
+    <>
+      {(editUser && (
+        <EditUserPage {...{ setEditUser, runFunction: toggle }} />
+      )) ||
+        (loading && <LoadingGif />) ||
+        (err && <p>Fel vid hämtning av profilsidan har inträffat</p>) || (
+          <Container className="profile-container">
+            <Row>
+              <Col className="offset-sm-2 offset-md-3 offset-lg-3">
+                <h1 className="line pb-1 my-3">Min Sida</h1>
+              </Col>
+            </Row>
+            <Row className="d-flex flex-column flex-lg-row-reverse justify-content-lg-between">
+              <Col lg={9}>
+                <ActiveBookings
+                  activeBookings={data?.active}
+                  setUpDate={setUpDate}
+                />
+                <ExpiredBookings expiredBookings={data?.expired} />
+              </Col>
+              <Col lg={3}>
+                <UserInfoCard {...{ setEditUser }} />
+              </Col>
+            </Row>
+          </Container>
+        )}
+    </>
   );
 }
 
