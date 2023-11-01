96c58cc764ff92307ba02dafe9d5233bfaa9756a
ledei <holybuzz@live.se>
2023-11-01 10:13:37 +0100

    added a error handler
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <oliver.andersson101@gmail.com>

--- client/src/components/ProfilePage/ActiveBookings.jsx
+++ client/src/components/ProfilePage/ActiveBookings.jsx
@@ -12,51 +12,43 @@ import Cancelbooking from "./CancelBooking.js";
 function ActiveBookings({ activeBookings, setUpDate }) {
   return (
     <>
-      <h6 className="offset-sm-2 offset-md-3 offset-lg-0">
-        Mina nuvarande bokningar
-      </h6>
-      <Row>
-        <Container className="d-flex flex-column align-items-center flex-lg-row flex-lg-wrap gap-3 m-0">
-          {activeBookings.map((booking, i) => (
-            <Col
-              lg={12}
-              className="active-booking-card d-flex flex-shrink-0 mb-2"
-              key={booking.referenceNumber + i}>
-              <Col xs={3} sm={3} lg={3}>
-                <Image src={booking.poster} fluid className="profile-poster" />
-              </Col>
-              <Col>
-                <h6 className="my-1 mx-2 booking-title">
-                  {booking.movieTitle}
-                </h6>
-                <p className="mb-0 mx-2">
-                  {getDateWithDay(booking.screeningStartTime)}
-                </p>
-                <p className="mb-0 mx-2">
-                  {getMovieEndTime(booking.screeningStartTime, booking.runTime)}
-                </p>
-                <p className="mb-0 mx-2">{`${booking.theaterName}, Plats-Stol: ${booking.seats}`}</p>
-                <p className="mb-1 mx-2">{`Pris: ${booking.priceSum} Kr`}</p>
-                <Col className="d-flex justify-content-between">
-                  <span className="ref-number mx-2">
-                    Bokningsnummer: {booking.referenceNumber}
-                  </span>
-                  <Col xs={3} className="card-btn-container ">
-                    <Button
-                      size="sm"
-                      className="mx-2 mb-1"
-                      onClick={() => {
-                        Cancelbooking(booking.bookingId, setUpDate);
-                      }}>
-                      Avboka
-                    </Button>
+      <h6 className="offset-sm-2 offset-md-3 offset-lg-0">Mina nuvarande bokningar</h6>
+      {activeBookings === undefined ? (
+        <p>Det finns framtida bookningar</p>
+      ) : (
+        <Row>
+          <Container className="d-flex flex-column align-items-center flex-lg-row flex-lg-wrap gap-3 m-0">
+            {activeBookings?.map((booking, i) => (
+              <Col lg={12} className="active-booking-card d-flex flex-shrink-0 mb-2" key={booking.referenceNumber + i}>
+                <Col xs={3} sm={3} lg={3}>
+                  <Image src={booking.poster} fluid className="profile-poster" />
+                </Col>
+                <Col>
+                  <h6 className="my-1 mx-2 booking-title">{booking.movieTitle}</h6>
+                  <p className="mb-0 mx-2">{getDateWithDay(booking.screeningStartTime)}</p>
+                  <p className="mb-0 mx-2">{getMovieEndTime(booking.screeningStartTime, booking.runTime)}</p>
+                  <p className="mb-0 mx-2">{`${booking.theaterName}, Plats-Stol: ${booking.seats}`}</p>
+                  <p className="mb-1 mx-2">{`Pris: ${booking.priceSum} Kr`}</p>
+                  <Col className="d-flex justify-content-between">
+                    <span className="ref-number mx-2">Bokningsnummer: {booking.referenceNumber}</span>
+                    <Col xs={3} className="card-btn-container ">
+                      <Button
+                        size="sm"
+                        className="mx-2 mb-1"
+                        onClick={() => {
+                          Cancelbooking(booking.bookingId, setUpDate);
+                        }}
+                      >
+                        Avboka
+                      </Button>
+                    </Col>
                   </Col>
                 </Col>
               </Col>
-            </Col>
-          ))}
-        </Container>
-      </Row>
+            ))}
+          </Container>
+        </Row>
+      )}
     </>
   );
 }
