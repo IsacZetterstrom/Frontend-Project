221a2e121940bccde10c4b8cf5dfec4433967b6d
ledei <holybuzz@live.se>
2023-11-01 10:32:21 +0100

    removed a hook and fixed some styling
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <oliver.andersson101@gmail.com>

--- client/src/components/Modals/ConfirmPopUpModal.jsx
+++ client/src/components/Modals/ConfirmPopUpModal.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Button, Col, Image, Row } from "react-bootstrap";
 import { formatDateOrTime, formatDateStringToSwedish, getMovieEndTime } from "../../utils/dateUtils";
-import { useNavigate, useParams } from "react-router-dom";
-import useFetchData from "../../hooks/useFetchData";
+import { useNavigate } from "react-router-dom";
 
 function ConfirmPopUpPage({ confirmationData, setToggle }) {
-  const { movieId } = useParams();
-  const { loading, err, data } = useFetchData(`/api/movies/${movieId}`);
   const navigate = useNavigate();
   const handleClick = () => {
     setToggle(false);
@@ -15,18 +12,18 @@ function ConfirmPopUpPage({ confirmationData, setToggle }) {
 
   return (
     <div className="confirm-wrapper-bg w-100">
-      <div className="confirm-wrapper d-flex flex-column align-items-center p-4">
-        <h1 className="mb-4">Tack för din bokning</h1>
+      <div className="confirm-wrapper d-flex flex-column  p-4">
+        <h1 className="mb-4 line-center">Tack för din bokning</h1>
 
         <Row>
           <Col xs={4}>
-            <Image src={data?.movie.Poster} alt="" fluid rounded className="max-200 max-300"></Image>
+            <Image src={confirmationData.poster} alt="" fluid rounded className="max-200 max-300"></Image>
           </Col>
           <Col xs={8} className="d-flex flex-column justify-content-between">
             <Row>
               <h2>{confirmationData.title}</h2>
               <p>{formatDateStringToSwedish(confirmationData.dateAndTime)}</p>
-              <p>{getMovieEndTime(confirmationData.dateAndTime, data?.movie.Runtime)}</p>
+              <p>{getMovieEndTime(confirmationData.dateAndTime, confirmationData.runtime)}</p>
               <p>
                 {confirmationData.saloon}, {confirmationData.seats}
               </p>
@@ -46,10 +43,11 @@ function ConfirmPopUpPage({ confirmationData, setToggle }) {
         </Row>
 
         <h3 className="my-3">Bokningsdetaljer har skickats till din mail</h3>
-
-        <Button variant="custom" onClick={() => handleClick()}>
-          Stäng
-        </Button>
+        <Row className="align-items-center justify-content-center">
+          <Button variant="custom" onClick={() => handleClick()}>
+            Stäng
+          </Button>
+        </Row>
       </div>
     </div>
   );
