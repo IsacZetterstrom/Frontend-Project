f27f0735ef7631b8e56b0d4869f0c7d5248cca1d
ledei <holybuzz@live.se>
2023-11-01 10:13:15 +0100

    took data from parent
    Co-authored-by: Niklas Nguyen <ledei@users.noreply.github.com>
    Co-authored-by: Sara Johansson <SaraJohanssonn@users.noreply.github.com>
    Co-authored-by: Oliver Andersson <oliver.andersson101@gmail.com>

--- client/src/components/Modals/ConfirmPopUpModal.jsx
+++ client/src/components/Modals/ConfirmPopUpModal.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Image, Row } from "react-bootstrap";
 import { formatDateOrTime, formatDateStringToSwedish, getMovieEndTime } from "../../utils/dateUtils";
 import { useNavigate, useParams } from "react-router-dom";
 import useFetchData from "../../hooks/useFetchData";
 
-function ConfirmPopUpPage({ popUpData, setToggle }) {
+function ConfirmPopUpPage({ confirmationData, setToggle }) {
   const { movieId } = useParams();
   const { loading, err, data } = useFetchData(`/api/movies/${movieId}`);
   const navigate = useNavigate();
@@ -12,6 +12,7 @@ function ConfirmPopUpPage({ popUpData, setToggle }) {
     setToggle(false);
     navigate("/");
   };
+
   return (
     <div className="confirm-wrapper-bg w-100">
       <div className="confirm-wrapper d-flex flex-column align-items-center p-4">
@@ -23,22 +24,22 @@ function ConfirmPopUpPage({ popUpData, setToggle }) {
           </Col>
           <Col xs={8} className="d-flex flex-column justify-content-between">
             <Row>
-              <h2>{popUpData.title}</h2>
-              <p>{formatDateStringToSwedish(popUpData.dateAndTime)}</p>
-              <p>{getMovieEndTime(popUpData.dateAndTime, popUpData.runtime)}</p>
+              <h2>{confirmationData.title}</h2>
+              <p>{formatDateStringToSwedish(confirmationData.dateAndTime)}</p>
+              <p>{getMovieEndTime(confirmationData.dateAndTime, data?.movie.Runtime)}</p>
               <p>
-                {popUpData.saloon}, {popUpData.seats}
+                {confirmationData.saloon}, {confirmationData.seats}
               </p>
               <p>
                 <span className="bold">Pris: </span>
-                {popUpData.priceSum}kr
+                {confirmationData.priceSum}kr
               </p>
             </Row>
             <Row>
-              <p>{popUpData.email}</p>
+              <p>{confirmationData.email}</p>
               <p>
                 <span className="bold">Bokningsnummer: </span>
-                {popUpData.bookingRef}
+                {confirmationData.bookingRef}
               </p>
             </Row>
           </Col>
