788839f07482be51567045219f6916c44aa22b00
Oliver Andersson <oliver.andersson101@gmail.com>
2023-11-07 15:39:18 +0100

    put creating ticket structure into util file

---
+++ client/src/utils/bookingPageUtils.js
@@ -0,0 +1,21 @@
+export function createTicketStructure(tickets, selectedSeats, screeningId) {
+
+    const data = {
+        tickets: [],
+    };
+      
+
+    for (const [key, value] of Object.entries(tickets)) {
+        for (let i = 0; i < value; i++) {
+          if (selectedSeats[i] === undefined) return;
+  
+          data.tickets.push({
+            Screening_id: screeningId,
+            Ticket_Type_id: key,
+            Seat_id: selectedSeats[i].Seat_id,
+          });
+        }
+    }
+
+    return data
+}
