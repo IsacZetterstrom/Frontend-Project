ed8f241af896ad4096035db22bdefedae7da93ee
KernelTestPilot <47748828+KernelTestPilot@users.noreply.github.com>
2023-11-07 12:52:46 +0100

    Switched to a faster movie api for fetching posters

--- Backend/src/models/tmdbModel.js
+++ Backend/src/models/tmdbModel.js
@@ -3,15 +3,16 @@ import "dotenv/config";
 
 async function getMovieInfo(titles) {
     const moviesArray = [];
-    const posterURL = "https://image.tmdb.org/t/p/original"
     for (const title of titles){
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.THEMOVIE_API_KEY}&query=${title}`;
+        const url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${title}`;
         const res = await fetch(url)
         const details =  await res.json();
-        const movieDetails = details.results[0];
-        movieDetails.full_poster_url = posterURL + movieDetails.poster_path;
-        console.log(movieDetails.full_poster_url)
-        moviesArray.push(movieDetails.full_poster_url)
+        if(details.Poster){
+            moviesArray.push(details.Poster)
+
+        }else{
+            moviesArray.push(null)
+        }
     }
     return moviesArray;
 }
