78324debb9c717c19e23c7c164dcd52d5a0258f4
Oliver Andersson <oliver.andersson101@gmail.com>
2023-11-07 16:11:07 +0100

    fixes

--- client/src/components/BookingPage/SeatPicker.jsx
+++ client/src/components/BookingPage/SeatPicker.jsx
@@ -18,7 +18,6 @@ function SeatPicker(props) {
     
     const rows = []
     const [hoveringSeats, setHoveringSeats] = useState([]);
-    
     const [selectSeveralSeats, setSelectSeveralSeats] = useState(true);
 
     // Take seats and put them in an array for each row to make rendering them out easier
@@ -32,16 +31,15 @@ function SeatPicker(props) {
 
 
     // Returns seats if they fit on the row and are not booked 
-    function getSeatsInRow(seat, row) {
+    function getSeatsInRow(seat, row, atFarRight) {
         const rowLength = row.length
         let seatIndex = row.indexOf(seat)
         const maxSeats = props.maxSeats
         
-
-        // Check so all seats fit to the right on the row    
+        // Check so all seats fit to the right on the row
         if(maxSeats + seatIndex > rowLength || seat.Booked === true) {
             // if not, go left one step
-            return getSeatsInRow(row[seatIndex-1], row)
+            return seatIndex === 0 ? [] : getSeatsInRow(row[seatIndex-1], row, true)
         } else {
 
             let newArr = [];
@@ -49,16 +47,16 @@ function SeatPicker(props) {
             // Check if any seat to the right is booked
             for (let i = 0; i < maxSeats; i++) {
                 
-                // If seat to right is booked, go left one step
+                // If seat to right is booked
                 if(row[seatIndex + i].Booked) {
-                    return seatIndex === 0 ? [] : getSeatsInRow(row[seatIndex-1], row)
+                    // Only go left if not at the far left already, or if we are at the far right 
+                    return seatIndex === 0 || atFarRight ? [] : getSeatsInRow(row[seatIndex-1], row)
                 }
                 
                 newArr.push(row[seatIndex + i])
             }
             
-            return newArr
-            
+            return newArr   
         }
     }
 
