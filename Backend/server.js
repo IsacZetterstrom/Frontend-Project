17dffef04c415e165d98a1f56f1b20b695c8d700
Oliver Andersson <oliver.andersson101@gmail.com>
2023-11-16 14:15:37 +0100

    serve dist folder from server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,18 @@ import publicRoutes from "./src/router/publicRoutes.js";
 import privateRoutes from "./src/router/privateRoutes.js";
 import authRoutes from "./src/router/authRoutes.js";
 import jwtFilter from "./src/middleware/jwtFilter.js";
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, '../client', 'dist');
 
 const app = Express();
 const port = 3050;
 
+app.use(Express.static(distPath));
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: false }));
 
@@ -14,4 +22,9 @@ app.use("/api", jwtFilter.loginCheck, publicRoutes);
 app.use("/profile", jwtFilter.verifyToken, privateRoutes);
 app.use("/auth", authRoutes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+});
+
 app.listen(port);
+
