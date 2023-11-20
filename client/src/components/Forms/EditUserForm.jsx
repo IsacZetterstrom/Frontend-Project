b80c933a9c77c18a076470d741cf6fc0d63bfb71
lollocodes <louise_johansson.94@hotmail.com>
2023-11-20 11:12:53 +0100

    changed example for phonenumber input

--- client/src/components/Forms/EditUserForm.jsx
+++ client/src/components/Forms/EditUserForm.jsx
@@ -43,7 +43,7 @@ function EditUserForm({ defaults, handleSubmit, setFormData, userData, runFuncti
             "Använd bara nummer"
           )}
         />
-        <span className="text-secondary">Exempel: 7XXXXXXXX</span>
+        <span className="text-secondary">Exempel: 07XXXXXXXX</span>
       </Col>
       <FormBtns
         {...{
